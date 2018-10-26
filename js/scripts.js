8373255df10afedc331a7a65e8258532034b42f4
Devin Sweeting <devinsweeting@gmail.com>
2018-10-26 14:11:50 -0700

    show output to DOM, change numSwap to strings

--- js/scripts.js
+++ js/scripts.js
@@ -8,37 +8,41 @@
 //   console.log(Array);
 // }
 
-function checkForZero(num) {
-  var numValue = num
+function numSwap(num) {
+  var numValue = num.toString()
   if ((num !== 0) && (num % 3 == 0)) {
-    return "Not a number!";
-  } else if (numValue === 1) {
+    return "I'm sorry, Dave. I'm afraid I can't do that.";
+  } else if ((numValue === "1") || (numValue === "11")) {
     return "Beep!"
-  } else if (numValue === 0) {
+  } else if ((numValue === "0") || (numValue === "10")) {
     return "Boop!"
   } else {
     return num;
-    console.log(numValue);
   }
+  console.log(numValue);
 }
 
 function translateToRobot(num) {
   var array = []
   for (var i = 0; i <= num; i++) {
-    array.push(i);
+    array.push(numSwap(i));
+  var arrayString = array.toString();
   }
   console.log(array)
+  console.log(arrayString);
+  return arrayString
 }
 
 // User Interface Logic
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
     event.preventDefault();
-    var valueSwap = checkForZero(parseInt($("input#number").val()));
-
+    var valueSwap = numSwap(parseInt($("input#number").val()));
     var result = translateToRobot(parseInt($("input#number").val()));
-    // var results = translateToRobot($("input#number").val());
     console.log(result)
     console.log(valueSwap);
+
+    $("#roboOutput").text(result);
+
   });
 });
