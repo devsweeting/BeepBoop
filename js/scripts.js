07ebdce74fa442fb5671b9c4c7502086675ee44e
Devin Sweeting <devinsweeting@gmail.com>
2018-10-26 10:33:33 -0700

    create user number in Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
-$(document).ready(function() {
+// Business logic
+function translate(number) {}
+  var numArray = []
+  for (var i = 0; i <= 10; i++) {
+    numArray.push(i)
+  }
+  console.log(numArray);
 
+  numArray.forEach(function(numbers)) {
+    if (number === 0) {numbers.push("Beep!")
+  console.log(numArray);
+    }
+  }
+
+function beep(num) {
+var beep = []
+  if (num === 0) {num = "Beep!"}
+  console.log(beep)
+  return beep.push(num)
+};
+
+
+
+// User Interface Logic
+$(document).ready(function() {
+  $("userInput").submit(function(event) {
+    event.preventDefault();
+    translate(parseInt($("input#number").val()));
+    beep(parseInt($("input#number").val()));
+
+
+    // $("#Output").text(numArray);
+  });
 });
