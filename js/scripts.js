0e3184ea79c3c96d7cc30bfbceedc61dad5c04d1
Devin Sweeting <devinsweeting@gmail.com>
2018-10-28 18:12:38 -0700

    Add parameters for letters, negative numbers, and too large of numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 
 // Business logic
 
-// var letters = ["a", "b", "c", "d"]
+var letters = ["a", "b", "c", "d"]
 function numSwap(num) {
   var numValue = num.toString()
   if ((num !== 0) && (num % 3 == 0)) {
     return "I'm sorry Dev I can't do that";
-  // } else if (numValue.includes(letters)) {
-  //   return "Thats not a number";
+  } else if (numValue.includes(letters)) {
+    return "Thats not a number";
   } else if (numValue.includes("1")) {
     return "BEEP";
   } else if (numValue.includes("0")) {
@@ -20,7 +20,15 @@ function numSwap(num) {
 
 
 function translateToRobot(num) {
-  var array = []
+  var array = [];
+  var userNumber = num.toString();
+  if (userNumber.includes("a", "b", "c", "d", "e","f","g","h","i", "j","k","l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z")) {
+    return "That's not a number";
+  }
+else if ((num <= -1) || (num >= 999999)) {
+    return "Beeeeeeeeeeeeeeeeeeeeeeeeep... cannot compute.. boop..";
+}
+ else {
   for (var i = 0; i <= num; i++) {
     array.push(numSwap(i));
   var arrayString = array.toString((" "));
@@ -29,7 +37,7 @@ function translateToRobot(num) {
   console.log(arrayString);
   return arrayString
 }
-
+}
 // User Interface Logic
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
