e621bfd0dae076789c674542d047b11355aec4d7
Devin Sweeting <devinsweeting@gmail.com>
2018-10-28 18:18:25 -0700

    clean up code & indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,8 @@
-
 // Business logic
-
-var letters = ["a", "b", "c", "d"]
 function numSwap(num) {
   var numValue = num.toString()
   if ((num !== 0) && (num % 3 == 0)) {
     return "I'm sorry Dev I can't do that";
-  } else if (numValue.includes(letters)) {
-    return "Thats not a number";
   } else if (numValue.includes("1")) {
     return "BEEP";
   } else if (numValue.includes("0")) {
@@ -23,20 +18,20 @@ function translateToRobot(num) {
   var array = [];
   var userNumber = num.toString();
   if (userNumber.includes("a", "b", "c", "d", "e","f","g","h","i", "j","k","l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z")) {
-    return "That's not a number";
+    return ".. That's not a *Beep* number...";
   }
-else if ((num <= -1) || (num >= 999999)) {
-    return "Beeeeeeeeeeeeeeeeeeeeeeeeep... cannot compute.. boop..";
-}
- else {
-  for (var i = 0; i <= num; i++) {
-    array.push(numSwap(i));
-  var arrayString = array.toString((" "));
+  else if ((num <= -1) || (num >= 999999)) {
+    return "Beeeeeeeeeeeeeeeeeeeeeeeeep... cannot compute.. boop.. server resetting";
   }
-  console.log(array)
-  console.log(arrayString);
-  return arrayString
-}
+  else {
+    for (var i = 0; i <= num; i++) {
+      array.push(numSwap(i));
+      var arrayString = array.toString((" "));
+    }
+    console.log(array)
+    console.log(arrayString);
+    return arrayString
+  }
 }
 // User Interface Logic
 $(document).ready(function() {
@@ -51,19 +46,3 @@ $(document).ready(function() {
 
   });
 });
-
-
-
-//
-// function Count (num) {
-//   var Array = [];
-//   for (var i = 0; i < num.length; i++) {
-//     Array.push(num[i]);
-//   }
-//   console.log(Array);
-// }
-
-// } else if ((numValue === "1") || (numValue === "11")) {
-//   return "Beep!"
-// } else if ((numValue === "0") || (numValue === "10")) {
-//   return "Boop!"
